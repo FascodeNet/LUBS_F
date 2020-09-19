aef36ed87eef32904990198b930db0ddba9d0051
kokkiemouse <kokkiemouse@gmail.com>
2020-09-19 12:26:21 +0900

    fixed login
    Signed-off-by: kokkiemouse <kokkiemouse@gmail.com>

--- channels/share/airootfs/root/customize_airootfs.sh
+++ channels/share/airootfs/root/customize_airootfs.sh
@@ -60,8 +60,8 @@ create_user "${username}" "${password}"
 
 
 # Set up auto login
-if [[ -f /etc/systemd/system/getty@tty1.service.d/autologin.conf ]]; then
-    sed -i s/%USERNAME%/"${username}"/g /etc/systemd/system/getty@tty1.service.d/autologin.conf
+if [[ -f /etc/systemd/system/getty@tty1.service.d/override.conf ]]; then
+    sed -i s/%USERNAME%/"${username}"/g /etc/systemd/system/getty@tty1.service.d/override.conf
 fi
 
 # Set to execute calamares without password as alter user.
