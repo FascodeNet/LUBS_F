e1d6d8ded7202398ae1cb33de643a85b4f5f8c7c
kokkiemouse <kokkiemouse@gmail.com>
2020-10-10 11:05:54 +0900

    translated
    Signed-off-by: kokkiemouse <kokkiemouse@gmail.com>

--- channels/share/airootfs/usr/share/calamares/branding/serene/lang/calamares-serene_ja.ts
+++ channels/share/airootfs/usr/share/calamares/branding/serene/lang/calamares-serene_ja.ts
@@ -4,13 +4,24 @@
 <context>
     <name>show</name>
     <message>
-        <source>&lt;b&gt;Welcome to SereneLinux&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Thank you for installing SereneLinux.
 This slide will show how to use SereneLinux.</source>
-        <translation type="unfinished"></translation>
+        <translation>SereneLinuxをインストールしていただき&lt;br/&gt;
+ありがとうございます。
+このスライドはSereneLinuxの使い方を乗せる予定です。</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Welcome to SereneLinux&lt;/b&gt;</source>
+        <translation>&lt;b&gt;ようこそ SereneLinuxへ&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <source>SereneLinux is using Xfce4 Desktop Enviroment.
+Xfce4 gives beautiful UI and freedom of customization.</source>
+        <translation>SereneLinux はXfce4を使用しています。Xfce 4は美しいUIで高度なカスタマイズが可能です。</translation>
+    </message>
+    <message>
+        <source>&lt;b&gt;Beautiful Desktop&lt;/b&gt;</source>
+        <translation>&lt;b&gt;美しいデスクトップ&lt;/b&gt;</translation>
     </message>
 </context>
 </TS>
