0ff4d4fd503fdd251d902fdceb685b47c065df08
kokkiemouse <kokkiemouse@fascode.net>
2020-12-28 23:49:33 +0900

    fixed lightdm?
    Signed-off-by: kokkiemouse <kokkiemouse@fascode.net>

--- channels/serene/airootfs/root/customize_airootfs_serene.sh
+++ channels/serene/airootfs/root/customize_airootfs_serene.sh
@@ -35,7 +35,8 @@ cp -f /etc/skel/Desktop/calamares.desktop /usr/share/applications/calamares.desk
 unlink /usr/share/backgrounds/images/default.png
 ln -s /usr/share/backgrounds/serene-wallpaper-1.png /usr/share/backgrounds/images/default.png
 
-echo -e "sed -i \"s/^autologin/#autologin/g\" /etc/lightdm/lightdm.conf" >> /usr/share/calamares/final-process
+echo -e "cp -f /lightdm.conf /etc/lightdm/lightdm.conf" >> /usr/share/calamares/final-process
+echo -e "rm -rf /lightdm.conf" >> /usr/share/calamares/final-process
 sed -i "s/- packages/- shellprocess\n  - removeuser\n  - packages/g" /usr/share/calamares/settings.conf
 sed -i "s/sb-shim/grub/g" /usr/share/calamares/modules/bootloader.conf
 sed -i "s/fedora/Serene Linux on Fedora/g" /usr/share/calamares/modules/bootloader.conf
